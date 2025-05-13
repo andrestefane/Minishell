76a1cf56c18c7bc51e3a603c7de5411961db85b9
Andrei <andres.stefanescu@gmail.com>
2025-05-13 19:02:45 +0200

    como redirreciono

--- Exec/Execute_parse_utils.c
+++ Exec/Execute_parse_utils.c
@@ -3,31 +3,25 @@
 
 void	parse_red_in(t_command *cmd, t_token **token)
 {
-	if (cmd->infile)
-		free(cmd->infile);
-	cmd->infile = ft_strdup((*token)->next->value);
-	cmd->is_heredoc = 0;
-	if (cmd->heredoc_file)
-	{
-		free(cmd->heredoc_file);
-		cmd->heredoc_file = NULL;
-	}
+	if (!(*token)->next)
+		exit_with_error("syntax error: no infile\n", 1, 2);
+	add_redir_to_cmd(cmd, T_RED_IN, (*token)->next->value);
 	*token = (*token)->next;
 }
 
 void	parse_red_out(t_command *cmd, t_token **token)
 {
-	if (cmd->outfile)
-		free(cmd->outfile);
-	cmd->outfile = ft_strdup((*token)->next->value);
+	if (!(*token)->next)
+		exit_with_error("syntax error: no outfile\n", 1, 2);
+	add_redir_to_cmd(cmd, T_RED_OUT, (*token)->next->value);
 	*token = (*token)->next;
 }
 
 void	parse_red_append(t_command *cmd, t_token **token)
 {
-	if (cmd->outfile)
-		free(cmd->outfile);
-	cmd->outfile = ft_strdup((*token)->next->value);
+	if (!(*token)->next)
+		exit_with_error("syntax error: no outfile\n", 1, 2);
+	add_redir_to_cmd(cmd, T_RED_APPEND, (*token)->next->value);
 	cmd->append = 1;
 	*token = (*token)->next;
 }
@@ -35,18 +29,14 @@ void	parse_red_append(t_command *cmd, t_token **token)
 void	parse_heredoc(t_command *cmd, t_token **token,
 	t_pipex *data, int *index)
 {
+	char	*filename;
+
 	if (!(*token)->next || !(*token)->next->value)
-		exit_with_error("heredoc: missing or invalid limiter\n", 1, 2);
-	if (cmd->heredoc_file)
-		free(cmd->heredoc_file);
-	handle_heredoc_in_command(cmd, (*token)->next->value, *index);
+		exit_with_error("heredoc: missing limiter\n", 1, 2);
+	filename = handle_heredoc_in_command(cmd, (*token)->next->value, *index);
+	add_redir_to_cmd(cmd, T_HEREDOC, filename);
 	cmd->is_heredoc = 1;
 	data->count_heredoc++;
-	if (cmd->infile)
-	{
-		free(cmd->infile);
-		cmd->infile = NULL;
-	}
 	(*index)++;
 	*token = (*token)->next;
 }
