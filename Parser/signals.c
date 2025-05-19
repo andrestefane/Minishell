dbefa842f010b1a846140fab5fab99f96633d9ac
Andrei <andres.stefanescu@gmail.com>
2025-05-19 20:13:41 +0200

    leaks corrios

--- Parser/signals.c
+++ Parser/signals.c
@@ -6,7 +6,7 @@
 /*   By: astefane <astefane@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 17:53:14 by alejaro2          #+#    #+#             */
-/*   Updated: 2025/05/19 19:30:02 by astefane         ###   ########.fr       */
+/*   Updated: 2025/05/19 20:13:31 by astefane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,3 +39,4 @@ void	heredoc_handler(int signum)
 	signal(SIGINT, SIG_DFL);
 	exit(EXIT_FAILURE);
 }
+
